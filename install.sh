8b557ed0024cf09ebdbed89efe7701e4b0312be6
Anton Lindroth <ntoonio@gmail.com>
2020-10-23 12:24:02 +0200

    Commented install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
+# Create a python venv and then install requirements
 if [ ! -d "venv" ]; then
 	python3 -m venv venv
 
 	venv/bin/pip install -r requirements.txt
 fi
 
+# Find the path to the manager
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Add mc_manager as a systemd service, with the correct path
 
 service=$(cat manager.service)
 
@@ -12,12 +17,16 @@ cat >/etc/systemd/system/mc_manager.service <<EOL
 ${service//"{path}"/$DIR}
 EOL
 
+# Create an "empty" server config file so that the manager can run
+
 cat >mc_servers_config.json << EOL
 {
 	"servers": [],
 	"update_interval": 24
 }
 EOL
+
+# Start the manager service
 
 systemctl daemon-reload
 systemctl enable mc_manager
